99ccb5415f0b6dbbe937097499960d705436b2d4
bigebro <bigeservice@gmail.com>
2021-05-18 23:28:54 +0800

    finished

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,62 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Date from '../components/date'
 
-export default function Home() {
+
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <div className="container">
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello this is BIlguun Enkhee. I'm software developer. I can speak english fluently and
+          contact me on <a href="twitter.com">Twitter</a>.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => ( 
+        <li className={utilStyles.listItem} key={id}>
+  <Link href={`/posts/${id}`}>
+    <a>{title}</a>
+  </Link>
+  <br />
+  <small className={utilStyles.lightText}>
+    <Date dateString={date} />
+  </small>
+</li>
+          ))}
+        </ul>
+      </section>
+
+    </Layout>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Read {' '}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
         </h1>
 
         <p className="description">
